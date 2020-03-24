d0e88898534dfb1ce3096e11ccc01d52b29a8cac
Daulet123 <180103212@stu.sdu.edu.kz>
2020-03-24 21:44:06 +0600

    The portal profile page

---
+++ hw10/src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,34 @@
+import {Component, ElementRef, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-sidenav',
+    templateUrl: './sidenav.component.html',
+    styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+    source='assets/icons/menu/arrows-right.svg';
+    open = false;
+
+    constructor(private el: ElementRef) { }
+
+    ngOnInit() {
+    }
+
+    toggle() {
+        this.open = !this.open;
+        const menuContainer = this.el.nativeElement.children[0];
+        if (this.open) {
+            menuContainer.classList.add('open');
+        } else {
+            menuContainer.classList.remove('open');
+        }
+    }
+
+    close() {
+        this.open = false;
+        const menuContainer = this.el.nativeElement.children[0];
+        menuContainer.classList.remove('open');
+       
+    }
+
+}
